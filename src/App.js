76680b9f66e09f364301daca53055343a6c31954
willowcheng <e.wingus@gmail.com>
2017-05-05 18:01:05 -0400

    Fetch data with fetch API

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ export default class App extends Component {
   
   constructor() {
     super();
-  } 
+    this.state = {
+      gifs: []
+    };
+  }
 
-  render() { 
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({ gifs: responseData.data });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+  render() {
+    console.log(this.state.gifs);
     return (
       <div>
         <div className="main-header">
