22cf2acc38ba83c85907c4b0f837fa216e8da20d
willowcheng <e.wingus@gmail.com>
2017-05-06 21:17:36 -0400

    Display search result

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-      gifs: []
+      gifs: [],
+      loading: true
     };
   } 
 
   componentDidMount() {
-
+    this.performSearch();
   }
-
-  performSearch = (query) => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}$limit=24&api_key=dc6zaTOxFJmzC`)
+  
+  performSearch = (query = 'cats') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
       .then(response => {
         this.setState({
-          gifs: response.data.data
+          gifs: response.data.data,
+          loading: false
         });
       })
       .catch(error => {
         console.log('Error fetching and parsing data', error);
-      });
+      });    
   }
-
+  
   render() { 
     console.log(this.state.gifs);
     return (
@@ -36,11 +38,15 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm onSearch={this.performSearch} />
+            <SearchForm onSearch={this.performSearch} />      
           </div>   
         </div>    
         <div className="main-content">
-          <GifList data={this.state.gifs} />
+          {
+            (this.state.loading)
+             ? <p>Loading...</p>
+             : <GifList data={this.state.gifs} />
+          }          
         </div>
       </div>
     );
